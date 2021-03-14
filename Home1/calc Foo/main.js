818457fa5dee0deb1c9fb0ce768bffe44ee505a9
AspireMobile <1ven970@gmail.com>
2021-03-14 17:06:13 +0300

    Calc foo

--- Home1/calc Foo/main.js
+++ Home1/calc Foo/main.js
@@ -1,12 +1,15 @@
 'use strict'
-let reg = "2+2+(3+1+(2-1*(5)))"
+let reg = "2+2+(3+1+(2-1*(6/2)))"
 const sign = ["+", "-", "*", "/", "=", "&divide;", "&times;", "."];
    reg = reg.split("").filter(el => el !==" ").join("");
 
 console.log(reg)
-//функция чтобы добраться до самых глубоких скоок
+
+
+//функция чтобы добраться до самых глубоких скоок  
+
+// переделать  через lastIndexOf 
 function brekit(str){
-  let myStr = str;
   let sub = str;
   if(!sub.substr(1).includes("(")){
     let newStr = sub.slice(1, sub.indexOf(")"));
@@ -17,82 +20,124 @@ function brekit(str){
     return brekit(sub.substr(1));
   }
 }
-reg = brekit(reg)
-reg = brekit(reg)
 
 
+reg = brekit(reg)
+console.log(reg)
+reg = brekit(reg)
+console.log(reg)
+reg = brekit(reg)
+console.log(reg)
+reg = brekit(reg)
+console.log(reg)
 
 
 
 function repl(str) {
-   if(!str.includes("(")){
-     return str 
-   }
-   else{
-     str = brekit(reg)
-     return repl(str.substr(1))
-    
-    }
+  return  str.split("").some(el => el !=="(") ? calculated(str) : repl(brekit(str))
+     
+  }
     
     
     //вставить все функцми
     
     
+
+
+
+
+function calculated(reg){
+reg = reg.split("")
+reg = creteArray(reg)
+searchDot(reg, ".");
+//минус ф-ция
+
+//reg = reg.map(el => isNaN(el) ? el : +el)
+// преобр
+
+operation(reg, sign[3], sign[2]); //сразу "* и /"
+operation(reg, sign[1], sign[0])
+
+  return reg.toString()
+  
+  
+  //функции
+  
+  //split строки
+  function creteArray(arr) {
+    let temp = ""
+    let changeArr = [];
+    arr.forEach(el => {
+      if (isNaN(el)) {
+        if (temp.length > 0) {
+          changeArr.push(temp);
+          temp = "";
+        }
+        changeArr.push(el);
+      }
+      else {
+        temp += el;
+      }
+    });
+    !!temp.length && changeArr.push(temp);
+   changeArr.filter((el, i) => changeArr[i] == '+' && changeArr[i+1] == '-' ? changeArr.splice(i, 1) : false)
+    return concatMinus(changeArr);
+  }
+  
+  function concatMinus(arr) {
+    arr.map((el, i) => {
+      if (el === "-" && arr[i-1] !== '+') {
+        arr.splice(i, 2, (-arr[i + 1]));
+        i !== 0 && arr.splice(i, 0, '+');
+      }
+    });
+    //console.log(arr, 'chan')
+    return arr;
+  }
+  //-------------
+  function searchDot(arr, sym) {
+    arr.map((el, i) => {
+      el === sym ? el = arr.splice(i - 1, 3, (arr[i - 1] + arr[i] + arr[i + 1])) : false;
+    });
+  }
+  
+  function operation(arr, op1, op2 = op1) {
+    arr.forEach(el => (el !== op1 || el !== op2) ? mathLogic(arr, op1, op2) : false);
+  }
+  //замена 3-х подряд идущих знаков в массиве на их решение
+  function mathLogic(arr, op1, op2 = op1) {
+    if (arr.length < 2) {
+      return arr;
+    }
+    let replaceEl;
+    arr.forEach((el, i) => {
+      if (arr[i] === op1 || arr[i] === op2) {
+        replaceEl = arr.splice(i - 1, 3, equals(arr[i - 1], arr[i], arr[i + 1]));
+      }
+    })
+  }
+  
+  //решение выражения
+  function equals(num1, op, num2) {
+    let newNum;
+    if (op === '+') {
+      newNum = parseFloat(num1) + parseFloat(num2);
+    }
+    if (op === '-') {
+      newNum = parseFloat(num1) - parseFloat(num2);
+    }
+    if (op === '*') {
+      newNum = parseFloat(num1) * parseFloat(num2);
+    }
+    if (op === '/') {
+      newNum = (parseFloat(num1) / parseFloat(num2));
+      if (newNum === Infinity) {
+        newNum = 0;
+      } else {
+        newNum === Math.floor(newNum) ? Math.floor(newNum) : newNum.toFixed(3);
+      }
+    }
+    return parseFloat(newNum)
+  }
+  
 }
-repl(reg)
-
-    //калькулятор
-    function calculated(arr) {
-      console.log(arr)
-      arr = arr.split("");
-      arr.map((el, i) => isNaN(el) || arr[i+1] === "-" ? el = arr.splice(i + 1, 2, ('+', arr[i+1] + arr[i + 2])) : el)
-      arr.includes(".") && searchDot(arr, sign[7])
-      operation(arr, sign[3], sign[2]); //сразу "* и /"
-      operation(arr, sign[1], sign[0]); //после "- и +"
-      return arr.join("")
-
-    }
-    //математическая логика для правильной последовательности операций
-    function operation(arr, op1, op2 = op1) {
-      arr.forEach(el => (el !== op1 || el !== op2) ? mathLogic(arr, op1, op2) : false);
-    }
-    //замена 3-х подряд идущих знаков в массиве на их решение
-    function mathLogic(arr, op1, op2 = op1) {
-      if (arr.length < 2) {
-        return arr.join("");
-      }
-      let replaceEl;
-      arr.forEach((el, i) => {
-        if (arr[i] === op1 || arr[i] === op2) {
-          replaceEl = arr.splice(i - 1, 3, equals(arr[i - 1], arr[i], arr[i + 1]));
-        }
-      })
-    }
-    function searchDot(arr, sym) {
-      arr.map((el, i) => {
-        el === sym ? el = arr.splice(i - 1, 3, (arr[i - 1] + arr[i] + arr[i + 1])) : false;
-      });
-    }
-    //решение выражения
-    function equals(num1, op, num2) {
-      if(num2 === "(") return num2
-      let newNum;
-      if (op === '+') {
-        return newNum = parseFloat(num1) + parseFloat(num2);
-      }
-      if (op === '-') {
-        return newNum = parseFloat(num1) - parseFloat(num2);
-      }
-      if (op === '*') {
-        return newNum = parseFloat(num1) * parseFloat(num2);
-      }
-      if (op === '/') {
-        newNum = (parseFloat(num1) / parseFloat(num2));
-        if (newNum === Infinity) {
-          return newNum = 0;
-        } else {
-          return newNum === Math.floor(newNum) ? Math.floor(newNum) : newNum.toFixed(3);
-        }
-      }
-    }
-/*console.log(calculated(reg))*/
