c1092b857dd840ebead40d282d3f2e604a589721
AspireMobile <1ven970@gmail.com>
2021-03-16 19:45:10 +0300

    practic

--- practic/main.js
+++ practic/main.js
@@ -6,68 +6,143 @@ const id = document.querySelector('#id');
 const btn = document.querySelector('#btn');
 const btnСreate = document.querySelector('#btnCreate');
 const par = document.querySelector('.par');
-const allInput = document.querySelectorAll('input');
-let arr = [];
-let counter = 0;
+const allInput = document.querySelectorAll('.input');
+const catalog = document.querySelector('.catalog')
+const headerCatalog = catalog.querySelector('.headerCatalog');
 
 
-const Horizont = {
+/*шаблон
+{
+imgUrl: "",
+price:  "",
+desk:   "",
+id :    "",
+}
+*/
+
+//mw микроволноака
+
+let arr = [
+
+{
+  imgUrl: "img/lgmw.jpg",
+  price: "250$",
+  desk: "Отличительная особенность микроволновок LG – удивительно точный подбор нужной мощности.",
+  id: 2,
+},
+
+
+ {
     imgUrl: "img/horizont.jpeg",
     price: '300$',
     desk: "LCD-телевизор с диагональю экрана 55 дюймов, Ultra HD разрешением и функцией SMART-TV",
-    id: 'tv01'
-}
+    id: 1,
+},
 
-const Atlant = {
-    imgUrl: "img/atlant.jpeg",
+{
+    imgUrl: "img/atlant.jpg",
     price: '500$',
-    desk: "Двухкамерный холодильник с дисплеем, электронное управление, зона свежести, класс A+",
-    id: 'fr01'
-}
+    desk: "Однокамерный холодильник с дисплеем, электронное управление, зона свежести, класс A+",
+    id: 3,
+},
+{
+  imgUrl: "img/midea.jpeg",
+  price: "150$",
+  desk: "25-литров объёма. Нержавейка внутри и снаружи. Местный производитель и гарантия",
+  id: 2,
+},
+{
+  imgUrl: "img/frindesit.jpg",
+  price: "500$",
+  desk: "Мощность, оснащение, размер.Цена.",
+  id: 2,
+},{
+  imgUrl: "img/tvsams.jpg",
+  price: "800$",
+  desk: "Мощность, оснащение, размер.Цена.",
+  id: 1,
+},];
 
-arr.push(Horizont)
-arr.push(Atlant)
 
 btn.addEventListener('click', createAndPush);
-btnCreate.addEventListener('click', createCard)
+btnCreate.addEventListener('click', createCard);
+headerCatalog.addEventListener('click', sortCatalog);
 
-function createAndPush(e) {
-    let el = {}; // Добавить условие если есть пуст ое поле>
-    el.imgUrl = imgUrl.value;
-    el.price = price.value;
-    el.desk = desk.value;
-    el.id = id.value;
-    arr.push(el)
-    setTimeout(() => clearForm(), 1000);
+function createAndPush() {
+    let obj = {}; // Доделать!!!!!
+    allInput.forEach(el => {
+      
+      if (!el.value) {
+        el.classList.add('required');
+        return
+      }
+      console.log(el.value)
+      if(el.value) {
+        obj.imgUrl = imgUrl.value;
+        obj.price = price.value;
+        obj.desk = desk.value;
+        obj.id = id.value;
+        arr.push(obj);
+        setTimeout(() => clearForm(), 1000);
+      }
+   })
 }
 
 function clearForm() {
-    allInput.forEach(el => el.value = '')
+    allInput.forEach(el => { 
+      el.value = ''
+      el.classList.contains('required') && el.classList.remove('required');
+    });
+
+
 }
 
 function createCard() {
-
-    arr.forEach(el => createElem(el))
+    arr.forEach(elem => createElem(elem));
+    catalog.classList.add('catalogActive');
 }
 
 function createElem(el) {
-    console.log(par.cloneNode)
     let card = document.createElement('div');
     card.classList.add('card');
-    card.dataset.id = arr[counter].id
-
+    card.dataset.id = el.id;
+    let wrapperImg = document.createElement('div');
+    wrapperImg.classList.add('wrapperImg');
     let img = document.createElement('img');
     img.src = el.imgUrl;
     img.textContent = el.imgUrl
     let price = document.createElement('div');
     price.classList.add('price');
     price.textContent = el.price;
-
-    let desk = document.createElement('div');
+    let desk = document.createElement('p');
     desk.classList.add('desk');
     desk.textContent = el.desk
-    card.append(img);
+    let btnBasket = document.createElement('button');
+    btnBasket.classList.add('btnBasket');
+    btnBasket.textContent = 'В корзину'
+    wrapperImg.append(img);
+    card.append(wrapperImg);
     card.append(price);
     card.append(desk);
+    card.append(btnBasket);
     par.append(card);
+}
+
+function sortCatalog(e){
+  let self = e.target;
+  const allCards = document.querySelectorAll('.card')
+
+  self.classList.contains('sortUp') && sortCol(par, allCards, 'data-id' );
+  
+  self.classList.contains('sortLow') && sortCol(par, allCards, 'data-id', false );
+  self.classList.contains('closeCatalog') && catalog.classList.remove('catalogActive');
+}
+
+function sortCol(parent, collection, data, type = true) {
+  let arr = [...collection];
+  type ? arr.sort((a, b) => a.getAttribute(data) - b.getAttribute(data)) : arr.sort((a, b) => b.getAttribute(data) - a.getAttribute(data));
+  let output = "";
+  arr.forEach(el => output += el.outerHTML);
+  console.log(arr)
+  return parent.innerHTML = output;
 }
