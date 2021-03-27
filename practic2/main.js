12e2521f2b5eeb49b41df8a59fc4ad9dbd01e341
Stanislau <1ven970@gmail.com>
2021-03-27 07:25:26 +0300

    swith

--- practic2/main.js
+++ practic2/main.js
@@ -5,24 +5,18 @@ const img = document.querySelector('#img');
 let scale = 1;
 let deg = 0;
 document.addEventListener('keydown', function(e) {
-    console.log(e.keyCode)
-    if (e.keyCode === 38) {
-        //up
-        img.style.transform = `scale(${scale += .1})  rotate(${deg}deg)`;
-
+    switch (e.keyCode) {
+        case 38:
+            img.style.transform = `scale(${scale += .1})  rotate(${deg}deg)`;
+            break;
+        case 37:
+            img.style.transform = `scale(${scale}) rotate(${deg -= 1}deg)`;
+            break;
+        case 40:
+            img.style.transform = `scale(${scale -= .1})  rotate(${deg}deg)`;
+            break;
+        case 39:
+            img.style.transform = `scale(${scale}) rotate(${deg+= 1}deg)`;
+            break;
     }
-    if (e.keyCode === 37) {
-        //left
-        img.style.transform = `scale(${scale}) rotate(${deg -= 1}deg)`;
-    }
-
-    if (e.keyCode === 40) {
-        //down
-        img.style.transform = `scale(${scale -= .1})  rotate(${deg}deg)`;
-    }
-
-    if (e.keyCode === 39) {
-        //right
-        img.style.transform = `scale(${scale}) rotate(${deg+= 1}deg)`;
-    }
-})
+});
