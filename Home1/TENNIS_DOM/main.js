1aa85cf82c38575186b064a68adcb6632401133e
Aspire212 <1ven970@gmail.com>
2021-04-24 16:14:15 +0300

    tennis

--- Home1/TENNIS_DOM/main.js
+++ Home1/TENNIS_DOM/main.js
@@ -4,13 +4,22 @@ const field = document.createElement('div');
 const ball = document.createElement('div');
 const leftRc = document.createElement('div');
 const rightRc = document.createElement('div');
+const btnPlay = document.createElement('button');
+
+btnPlay.textContent = 'Играть!';
 
 ball.classList.add('ball');
 leftRc.classList.add('leftRc');
 rightRc.classList.add('rightRc');
 
+const appStyle = {
+    display: 'flex',
+    justifyContent: 'center',
+    alignItems: 'center',
+    flexDirection: 'column',
+};
+
 const fieldStyle = {
-    //border: '1px solid red',
     background: 'yellow',
     width: '600px',
     height: '400px',
@@ -38,56 +47,60 @@ class RcStyle {
     }
 };
 
+const btnStyle = {
+    width: '120px',
+    height: '50px',
+    fontSize: '30px',
+    margin: '40px',
+};
+
+
 const leftRcStyle = new RcStyle(0, 'green');
 const rightRcStyle = new RcStyle('580px', 'lightblue');
 
-const randomDiap = (n, m) => Math.floor(Math.random() * (m - n + 1)) + n
+const randomDiap = (n, m) => Math.floor(Math.random() * (m - n + 1)) + n;
 
-
+setStyle(appStyle, app);
 setStyle(fieldStyle, field);
 setStyle(ballStyle, ball);
 setStyle(leftRcStyle, leftRc);
 setStyle(rightRcStyle, rightRc);
+setStyle(btnStyle, btnPlay);
 
 field.append(ball);
 field.append(rightRc);
 field.append(leftRc);
 app.append(field);
+app.append(btnPlay);
 
 const ballMove = {
     x : noZero(-2, 2),
     y : noZero(-2, 2),
     startX : 0,
     startY : 0,
-    speed : randomDiap(7, 10),
+    speed : randomDiap(8, 10),
 }
 
+let timer; 
 
-
-
-let timer;
-timer = setInterval(() => move(), ballMove.speed)
+btnPlay.addEventListener('click', () => timer = setInterval(() => move(), ballMove.speed));
 
 function move(){
     const dataBall = {
-        top : ball.getBoundingClientRect().top,
-        left : ball.getBoundingClientRect().left,
+        top : ball.getBoundingClientRect().top - field.getBoundingClientRect().top,
+        left : ball.getBoundingClientRect().left - field.getBoundingClientRect().left,
         size : ball.getBoundingClientRect().height,
     }
-    if (dataBall.top + dataBall.size >= 400 || dataBall.top <= 0) {
+    if (dataBall.top + dataBall.size >= field.offsetHeight || dataBall.top <= 0) {
         ballMove.y = -ballMove.y;
     }
-    if (dataBall.left + dataBall.size >= 600 || 
+    if (dataBall.left + dataBall.size >= field.offsetWidth || 
     dataBall.left <= 0) {
         ballMove.x = -ballMove.x;
     }
-    console.log(ball.offsetLeft)
     ballMove.startY += ballMove.y;
     ballMove.startX += ballMove.x;
-    ball.style.transform = `translate(${ballMove.startX}px, ${ballMove.startY}px)`;
-    
-    
-
+    ball.style.transform = `translate(${ballMove.startX}px, ${ballMove.startY}px)`
 }
 
 function setStyle(objStyle, parent) {
